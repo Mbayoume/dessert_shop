57b78442ccd1d7245f38a5b51ae04669e1d49ffc
bayoume <mohamed.bayoumi@raneen.com>
2025-06-02 16:45:20 +0300

    initial documentation file and webapack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // Injects CSS into DOM
+          'css-loader',   // Turns CSS into JS
+          'postcss-loader', // Adds vendor prefixes
+          'sass-loader',  // Compiles SCSS to CSS
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // Cleans dist folder before rebuild
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Uses our HTML as template
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+};
