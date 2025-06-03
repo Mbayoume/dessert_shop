fa86cef9993c830f1d3d7dbc9b143c8aa24c7f4b
bayoume <mohamed.bayoumi@raneen.com>
2025-06-03 18:52:18 +0300

    set the cart and the products containers

---
+++ src/js/services/productService.js
@@ -0,0 +1,47 @@
+const desserts = [
+  {
+    name: "Waffle with Berries",
+    type: "Waffle",
+    price: 6.50
+  },
+  {
+    name: "Vanilla Bean Crème Brûlée",
+    type: "Crème Brûlée",
+    price: 7.00
+  },
+  {
+    name: "Macaron Mix of Five",
+    type: "Macaron",
+    price: 8.00
+  },
+  {
+    name: "Classic Tiramisu",
+    type: "Tiramisu",
+    price: 5.50
+  },
+  {
+    name: "Pistachio Baklava",
+    type: "Baklava",
+    price: 4.00
+  },
+  {
+    name: "Lemon Meringue Pie",
+    type: "Pie",
+    price: 5.00
+  },
+  {
+    name: "Red Velvet Cake",
+    type: "Cake",
+    price: 4.50
+  },
+  {
+    name: "Salted Caramel Brownie",
+    type: "Brownie",
+    price: 4.50
+  },
+  {
+    name: "Vanilla Panna Cotta",
+    type: "Panna Cotta",
+    price: 6.50
+  }
+];
